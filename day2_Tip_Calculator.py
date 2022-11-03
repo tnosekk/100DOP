ac90f9da28f8ffd6bd925fcc9018ece71307c7c3
tnosekk <117302895+tnosekk@users.noreply.github.com>
2022-11-03 10:49:36 +0100

    Update day2_Tip_Calculator.py

--- day2_Tip_Calculator.py
+++ day2_Tip_Calculator.py
@@ -1,12 +1,12 @@
 print("Welcome to the tip calculator!")
 
-bill = input("What was the total bill? $")
-tip = input("How much tip would you like to give? 10, 12, or 15? ")
-people = input("How meny people to split the bill? ")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How meny people to split the bill? "))
 
-bill_per_person = round(float(bill)/int(people), 3)
-tip_per_person = round((float(bill)*int(tip))/100/int(people),2)
-total_per_person = round(float(bill_per_person)+float(tip_per_person),2)
+bill_per_person = round(bill / people, 3)
+tip_per_person = round((bill * tip)/100/people, 2)
+total_per_person = round(bill_per_person + tip_per_person, 2)
 
 message = f"Each person should pay: ${total_per_person}"
 print(message)
