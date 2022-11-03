178247bdc439c24073b484fe2a9c076b06ce1f95
tnosekk <117302895+tnosekk@users.noreply.github.com>
2022-11-03 10:53:43 +0100

    Update day2_Tip_Calculator.py

--- day2_Tip_Calculator.py
+++ day2_Tip_Calculator.py
@@ -7,6 +7,8 @@ people = int(input("How meny people to split the bill? "))
 bill_per_person = round(bill / people, 3)
 tip_per_person = round((bill * tip)/100/people, 2)
 total_per_person = round(bill_per_person + tip_per_person, 2)
+total_per_person = "{:.2f}".format(total_per_person)
+
 
 message = f"Each person should pay: ${total_per_person}"
 print(message)
